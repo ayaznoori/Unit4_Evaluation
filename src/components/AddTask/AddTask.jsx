608dd2fd0c8c0a22d0dcfec392148665a6f3b41e
ayaznoori <ayaz.noori15@gmail.com>
2022-05-23 17:01:22 +0530

    s

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,30 +1,27 @@
 import React, { useState } from "react";
+import Tasks from "../Tasks/Tasks";
 import styles from "./addTask.module.css";
 
 const AddTask = () => {
-  let con={};
   // NOTE: do not delete `data-cy` key value pair
   const [itvalue,setitvalue]=useState("");
   const [itarr,setitarr]=useState([]);
   const itemadding=(e)=>{
        setitvalue(e.target.value);
-       
   }
   const adding=()=>{
-   
     if(itvalue.length>0){
       setitarr([...itarr,itvalue]);
-      console.log(itarr);
       setitvalue("");
+      Tasks();
     }
-  
   }
-  
+  localStorage.setItem("data",JSON.stringify(itarr));
   return (
     <div className={styles.todoForm}>
       <input data-cy="add-task-input" type="text" className={styles.inputbox}  placeholder="Add here..."
        onChange={itemadding}/>
-      <button data-cy="add-task-button" className={styles.inputbutton} onClick={adding}>+</button>
+      <button data-cy="add-task-button" className={styles.inputbutton} onClick={adding} >+</button>
     </div>
   );
  
--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./tasks.module.css";
+ 
 
-const Tasks = (props) => {
+const Tasks = () => {
   // NOTE: do not delete `data-cy` key value pair
+  let arr=JSON.parse(localStorage.getItem("data"))||[];
+  const [arrvalue,setarrvalue]=useState(arr);
+ 
   return (
     <>
       <ul data-cy="tasks" className={styles.tasks}>
-        { console.log(props.con+"hi")}
+        { arr.map((ele)=>{return <li>{ele}</li>})}
       </ul>
       <div data-cy="tasks-empty" className={styles.empty}>
         {/* Show when No Tasks are present */}
-      </div>
+      </div> 
     </>
+    
   );
 };
 
